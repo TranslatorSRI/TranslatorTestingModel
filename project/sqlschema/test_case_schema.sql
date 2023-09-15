e2bf341cf8edba5fc1e5bf1cbe45fbde97b23775
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-09-15 10:46:34 -0700

    remove generic person attributes from cookiecutter setup

--- project/sqlschema/test_case_schema.sql
+++ project/sqlschema/test_case_schema.sql
@@ -1,23 +1,5 @@
 
 
-CREATE TABLE "NamedThing" (
-	id TEXT NOT NULL, 
-	name TEXT, 
-	description TEXT, 
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE "TestCase" (
-	id TEXT NOT NULL, 
-	name TEXT, 
-	description TEXT, 
-	primary_email TEXT, 
-	birth_date DATE, 
-	age_in_years INTEGER, 
-	vital_status VARCHAR(7), 
-	PRIMARY KEY (id)
-);
-
 CREATE TABLE "TestCaseCollection" (
 	entries TEXT, 
 	PRIMARY KEY (entries)
--- src/test_case_schema/datamodel/test_case_schema.py
+++ src/test_case_schema/datamodel/test_case_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from test_case_schema.yaml by pythongen.py version: 0.0.1
-# Generation date: 2023-09-15T10:44:07
+# Generation date: 2023-09-15T10:46:18
 # Schema: test-case-schema
 #
 # id: https://w3id.org/TranslatorSRI/test-case-schema
@@ -21,8 +21,8 @@ from linkml_runtime.utils.formatutils import camelcase, underscore, sfx
 from linkml_runtime.utils.enumerations import EnumDefinitionImpl
 from rdflib import Namespace, URIRef
 from linkml_runtime.utils.curienamespace import CurieNamespace
-from linkml_runtime.linkml_model.types import Date, Integer, String, Uriorcurie
-from linkml_runtime.utils.metamodelcore import URIorCURIE, XSDDate
+from linkml_runtime.linkml_model.types import String, Uriorcurie
+from linkml_runtime.utils.metamodelcore import URIorCURIE
 
 metamodel_version = "1.7.0"
 version = None
@@ -31,7 +31,6 @@ version = None
 dataclasses._init_fn = dataclasses_init_fn_with_kwargs
 
 # Namespaces
-PATO = CurieNamespace('PATO', 'http://purl.obolibrary.org/obo/PATO_')
 BIOLINK = CurieNamespace('biolink', 'https://w3id.org/biolink/')
 EXAMPLE = CurieNamespace('example', 'https://example.org/')
 LINKML = CurieNamespace('linkml', 'https://w3id.org/linkml/')
@@ -43,47 +42,10 @@ DEFAULT_ = TEST_CASE_SCHEMA
 # Types
 
 # Class references
-class NamedThingId(URIorCURIE):
-    pass
 
 
-class TestCaseId(NamedThingId):
-    pass
 
-
-@dataclass
-class NamedThing(YAMLRoot):
-    """
-    A generic grouping for any identifiable entity
-    """
-    _inherited_slots: ClassVar[List[str]] = []
-
-    class_class_uri: ClassVar[URIRef] = SCHEMA.Thing
-    class_class_curie: ClassVar[str] = "schema:Thing"
-    class_name: ClassVar[str] = "NamedThing"
-    class_model_uri: ClassVar[URIRef] = TEST_CASE_SCHEMA.NamedThing
-
-    id: Union[str, NamedThingId] = None
-    name: Optional[str] = None
-    description: Optional[str] = None
-
-    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        if self._is_empty(self.id):
-            self.MissingRequiredField("id")
-        if not isinstance(self.id, NamedThingId):
-            self.id = NamedThingId(self.id)
-
-        if self.name is not None and not isinstance(self.name, str):
-            self.name = str(self.name)
-
-        if self.description is not None and not isinstance(self.description, str):
-            self.description = str(self.description)
-
-        super().__post_init__(**kwargs)
-
-
-@dataclass
-class TestCase(NamedThing):
+class TestCase(YAMLRoot):
     """
     Represents a TestCase
     """
@@ -93,32 +55,6 @@ class TestCase(NamedThing):
     class_class_curie: ClassVar[str] = "test_case_schema:TestCase"
     class_name: ClassVar[str] = "TestCase"
     class_model_uri: ClassVar[URIRef] = TEST_CASE_SCHEMA.TestCase
-
-    id: Union[str, TestCaseId] = None
-    primary_email: Optional[str] = None
-    birth_date: Optional[Union[str, XSDDate]] = None
-    age_in_years: Optional[int] = None
-    vital_status: Optional[Union[str, "PersonStatus"]] = None
-
-    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        if self._is_empty(self.id):
-            self.MissingRequiredField("id")
-        if not isinstance(self.id, TestCaseId):
-            self.id = TestCaseId(self.id)
-
-        if self.primary_email is not None and not isinstance(self.primary_email, str):
-            self.primary_email = str(self.primary_email)
-
-        if self.birth_date is not None and not isinstance(self.birth_date, XSDDate):
-            self.birth_date = XSDDate(self.birth_date)
-
-        if self.age_in_years is not None and not isinstance(self.age_in_years, int):
-            self.age_in_years = int(self.age_in_years)
-
-        if self.vital_status is not None and not isinstance(self.vital_status, PersonStatus):
-            self.vital_status = PersonStatus(self.vital_status)
-
-        super().__post_init__(**kwargs)
 
 
 @dataclass
@@ -133,32 +69,18 @@ class TestCaseCollection(YAMLRoot):
     class_name: ClassVar[str] = "TestCaseCollection"
     class_model_uri: ClassVar[URIRef] = TEST_CASE_SCHEMA.TestCaseCollection
 
-    entries: Optional[Union[Dict[Union[str, TestCaseId], Union[dict, TestCase]], List[Union[dict, TestCase]]]] = empty_dict()
+    entries: Optional[Union[Union[dict, TestCase], List[Union[dict, TestCase]]]] = empty_list()
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        self._normalize_inlined_as_dict(slot_name="entries", slot_type=TestCase, key_name="id", keyed=True)
+        if not isinstance(self.entries, list):
+            self.entries = [self.entries] if self.entries is not None else []
+        self.entries = [v if isinstance(v, TestCase) else TestCase(**as_dict(v)) for v in self.entries]
 
         super().__post_init__(**kwargs)
 
 
 # Enumerations
-class PersonStatus(EnumDefinitionImpl):
 
-    ALIVE = PermissibleValue(
-        text="ALIVE",
-        description="the person is living",
-        meaning=PATO["0001421"])
-    DEAD = PermissibleValue(
-        text="DEAD",
-        description="the person is deceased",
-        meaning=PATO["0001422"])
-    UNKNOWN = PermissibleValue(
-        text="UNKNOWN",
-        description="the vital status is not known")
-
-    _defn = EnumDefinition(
-        name="PersonStatus",
-    )
 
 # Slots
 class slots:
@@ -173,21 +95,5 @@ slots.name = Slot(uri=SCHEMA.name, name="name", curie=SCHEMA.curie('name'),
 slots.description = Slot(uri=SCHEMA.description, name="description", curie=SCHEMA.curie('description'),
                    model_uri=TEST_CASE_SCHEMA.description, domain=None, range=Optional[str])
 
-slots.primary_email = Slot(uri=SCHEMA.email, name="primary_email", curie=SCHEMA.curie('email'),
-                   model_uri=TEST_CASE_SCHEMA.primary_email, domain=None, range=Optional[str])
-
-slots.birth_date = Slot(uri=SCHEMA.birthDate, name="birth_date", curie=SCHEMA.curie('birthDate'),
-                   model_uri=TEST_CASE_SCHEMA.birth_date, domain=None, range=Optional[Union[str, XSDDate]])
-
-slots.age_in_years = Slot(uri=TEST_CASE_SCHEMA.age_in_years, name="age_in_years", curie=TEST_CASE_SCHEMA.curie('age_in_years'),
-                   model_uri=TEST_CASE_SCHEMA.age_in_years, domain=None, range=Optional[int])
-
-slots.vital_status = Slot(uri=TEST_CASE_SCHEMA.vital_status, name="vital_status", curie=TEST_CASE_SCHEMA.curie('vital_status'),
-                   model_uri=TEST_CASE_SCHEMA.vital_status, domain=None, range=Optional[Union[str, "PersonStatus"]])
-
 slots.testCaseCollection__entries = Slot(uri=TEST_CASE_SCHEMA.entries, name="testCaseCollection__entries", curie=TEST_CASE_SCHEMA.curie('entries'),
-                   model_uri=TEST_CASE_SCHEMA.testCaseCollection__entries, domain=None, range=Optional[Union[Dict[Union[str, TestCaseId], Union[dict, TestCase]], List[Union[dict, TestCase]]]])
-
-slots.TestCase_primary_email = Slot(uri=SCHEMA.email, name="TestCase_primary_email", curie=SCHEMA.curie('email'),
-                   model_uri=TEST_CASE_SCHEMA.TestCase_primary_email, domain=TestCase, range=Optional[str],
-                   pattern=re.compile(r'^\S+@[\S+\.]+\S+'))
+                   model_uri=TEST_CASE_SCHEMA.testCaseCollection__entries, domain=None, range=Optional[Union[Union[dict, TestCase], List[Union[dict, TestCase]]]])
