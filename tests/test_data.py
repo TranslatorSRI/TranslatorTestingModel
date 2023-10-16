7c8dffdf342685881a78cf0131ecd1f1dfcb2a48
richardbruskiewich <richard.bruskiewich@delphinai.com>
2023-10-16 16:33:28 -0700

    Fixed tests module to call the proper schema class, but the test itself still fails (since I removed it... need to restore something here...)

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,7 +4,7 @@ import glob
 import unittest
 
 from linkml_runtime.loaders import yaml_loader
-from test_case_schema.datamodel.test_case_schema import TestCase
+from translator_testing_model.datamodel.translator_testing_model import TestCase
 
 ROOT = os.path.join(os.path.dirname(__file__), '..')
 DATA_DIR = os.path.join(ROOT, "src", "data", "examples")
@@ -16,7 +16,7 @@ class TestData(unittest.TestCase):
     """Test data and datamodel."""
 
     def test_data(self):
-        """Date test."""
+        """TestCase test."""
         for path in EXAMPLE_FILES:
             obj = yaml_loader.load(path, target_class=TestCase)
             assert obj
