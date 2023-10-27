eafa601ed151732d379e5d4de722d28b7f6d9045
richardbruskiewich <richard.bruskiewich@delphinai.com>
2023-10-27 15:53:50 -0700

    Another iteration on the test suite script (isn't persisted to Tests yet...)

--- scripts/test_suite_generator.py
+++ scripts/test_suite_generator.py
@@ -2,10 +2,12 @@
 """
 CLI Script to generate a TestSuite of TestCases from specified TestAssets
 """
-from typing import List
+from typing import Optional, List
 from argparse import ArgumentParser
 from urllib.parse import urlparse
 from linkml_runtime.loaders import tsv_loader, json_loader, yaml_loader
+from linkml_runtime.dumpers import  json_dumper
+
 from src.translator_testing_model.datamodel.translator_testing_model import (
     TestAsset,
     TestCase,
@@ -21,20 +23,44 @@ def url_type(arg):
 
 
 def main(args):
-    test_suite_id = args["test_suite_id"]
-    test_suite_name = args["test_suite_name"]
-    test_assets_url = args["test_assets_url"]
-    test_assets_format = test_assets_url.split('.')[-1]
-    test_suite_url = args["test_suite_url"]
+    test_suite_id:str = args["test_suite_id"]
+    test_suite_name: str = args["test_suite_name"]
 
+    test_assets_url: str = args["test_assets_url"]
+    test_assets_format: str = test_assets_url.split('.')[-1]
+
+    test_suite_url: str = args["test_suite_url"]
+    # sanity check - remove any trailing slash in URL; anything else will really screw things up for now!
+    test_suite_url = test_suite_url.rstrip("/")
+
+    # Load the targeted TestAssets
     if test_assets_format == "tsv":
-        test_assets: List[TestAsset] = tsv_loader.load(test_assets_url, target_class=TestAsset)
+        test_assets = tsv_loader.load_any(test_assets_url, target_class=TestAsset)
     elif test_assets_format == "json":
-        test_assets: List[TestAsset] = json_loader.load(test_assets_url, target_class=TestAsset)
+        test_assets = json_loader.load_any(test_assets_url, target_class=TestAsset)
     elif test_assets_format == "yaml":
-        test_assets: List[TestAsset] = yaml_loader.load(test_assets_url, target_class=TestAsset)
+        test_assets = yaml_loader.load_any(test_assets_url, target_class=TestAsset)
     else:
         raise RuntimeError(f"Unknown TestAsset file format: {test_assets_format}")
+
+    # TODO: Filter out TestAssets if required (need more CLI params and methodology
+
+    # Build the list of TestCases
+    test_cases: List[TestCase] = list()
+    n = 0
+    for asset in test_assets:
+        n += 1
+        tc_id = f"TestCase:{n}"
+        test_case = TestCase(id=tc_id, test_assets=asset)
+        test_cases.append(test_case)
+
+    test_suite = TestSuite(id=test_suite_id, name=test_suite_name, test_cases=test_cases)
+
+    test_suite_text: str = json_dumper.dumps(element=test_suite)
+
+    # TODO: Publish the new Test Suite as a JSON file 'blob' in the target (Github) location
+    test_suite_file = f"{test_suite_url}/{test_suite_name.strip()}.json"
+    pass
 
 
 def cli():
