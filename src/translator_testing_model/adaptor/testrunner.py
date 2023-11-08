d1673ba46f01c320dfffea049b3528c33d5ba552
richardbruskiewich <richard.bruskiewich@delphinai.com>
2023-11-07 20:12:23 -0800

    Another braintorming iteration of the TestRunner facade library.

--- src/translator_testing_model/adaptor/testrunner.py
+++ src/translator_testing_model/adaptor/testrunner.py
@@ -2,62 +2,56 @@
 This module provides a high level parent class interface/API
 for Translator TestRunner implementations.
 """
-from typing import Union, List
+from typing import Union, List, Optional, Dict
 from datetime import datetime
 
 from src.translator_testing_model.datamodel.pydanticmodel import (
+    TestRunnerConfiguration,
     TestEntity,
     TestCase,
     TestCaseResult,
-    TestCaseResultEnum
+    TestCaseResultEnum,
+    TestRunSession
 )
-
-
-class TestResults:
-    """
-    Wrapper of a Translator TestRunner result
-    """
-
-    def __init__(self, test_id: str):
-        """
-        TestResult constructor
-        """
-        self.id = test_id
-        self.test_case_results: List[TestCaseResult] = list()
-
-    def add_test_case_result(self, test_case: TestCase, test_case_result: TestCaseResultEnum):
-        self.test_case_results.append(
-            TestCaseResult(
-                test_suite_id=self.id,
-                test_case=test_case,
-                test_case_result=test_case_result,
-                timestamp=datetime.now()
-            )
-        )
-
-    def get_test_case_results(self) -> List[TestCaseResult]:
-        return self.test_case_results
 
 
 class TestRunner:
     """
-    Abstract Wrapper Class for "plug and play" TestRunner implementations
-    for the Translator Testing Infrastructure. Complying with this
-    specification allows for easy execution of TestRunners by the TestHarness.
+    Abstract Wrapper Class for "plug and play" TestRunner implementations for
+    the Translator Testing Infrastructure. Complying with this specification
+    facilitates uniform execution of TestRunners by the TestHarness.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, config: TestRunnerConfiguration):
         """
-        TestRunner constructor
-        """
-        raise NotImplementedError
+        Constructor for an instance of TestRunner.
 
-    def run(self, tests: Union[TestEntity, List[TestEntity]]) -> TestResults:
+        :param config: TestRunnerConfiguration, general run configuration parameters for a specified TestRunner
         """
+        self.config = config
+
+        # catalog of registered sessions of the test runner.
+        self.sessions: Dict[
+            str,            # session_id
+            TestRunSession  # session object
+        ] = dict()
+
+    def get_session(self, session_id: str) -> Optional[TestRunSession]:
+        """
+        Returns the TestRunner session corresponding to the given session identifier
+        :param session_id: valid session identifier of registered TestRunner session
+        :return: Optional[TestRunSession], session of a valid extant instance of TestRunner if known; None otherwise
+        """
+        return self.sessions[session_id] if session_id in self.sessions else None
+
+    def run(self, tests: Union[TestEntity, List[TestEntity]]) -> TestRunSession:
+        """
+        Method to set up asynchronous running of the specified collection of tests
+        by the configured TestRunner, returning the test results.
 
         :param tests: Union[TestEntity, List[TestEntity]], may be any reasonable (TestRunner-specific) -
                       possibly a List of - instance(s) of test data entity but generally constrained to
                       be of a TestSuite, TestCase or TestAsset derived data type.
-        :return: TestResults, data wrapper for (full) reporting of the (TestSuite of) TestCase result(s).
+        :return: TestRunSession, data wrapper for an instance of TestRunner running the provided tests.
         """
         raise NotImplementedError
